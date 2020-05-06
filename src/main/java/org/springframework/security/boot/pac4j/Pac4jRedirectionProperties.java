43703d97e7f590228886a00318b79ecc4f2118e5
wandl <hnxyhcwdl1003@163.com>
2020-05-06 15:24:34 +0800

    xh

--- src/main/java/org/springframework/security/boot/pac4j/Pac4jRedirectionProperties.java
+++ src/main/java/org/springframework/security/boot/pac4j/Pac4jRedirectionProperties.java
@@ -18,9 +18,6 @@ package org.springframework.security.boot.pac4j;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.security.web.util.UrlUtils;
-import org.springframework.util.Assert;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -57,71 +54,5 @@ public class Pac4jRedirectionProperties {
 	private boolean alwaysUseDefaultTargetUrl = false;
 	
 	private boolean useReferer = false;
-	
-	/**
-	 * Supplies the default target Url that will be used if no saved request is found or
-	 * the {@code alwaysUseDefaultTargetUrl} property is set to true. If not set, defaults
-	 * to {@code /}.
-	 *
-	 * @return the defaultTargetUrl property
-	 */
-	protected final String getDefaultTargetUrl() {
-		return defaultTargetUrl;
-	}
-
-	/**
-	 * Supplies the default target Url that will be used if no saved request is found in
-	 * the session, or the {@code alwaysUseDefaultTargetUrl} property is set to true. If
-	 * not set, defaults to {@code /}. It will be treated as relative to the web-app's
-	 * context path, and should include the leading <code>/</code>. Alternatively,
-	 * inclusion of a scheme name (such as "http://" or "https://") as the prefix will
-	 * denote a fully-qualified URL and this is also supported.
-	 *
-	 * @param defaultTargetUrl
-	 */
-	public void setDefaultTargetUrl(String defaultTargetUrl) {
-		Assert.isTrue(UrlUtils.isValidRedirectUrl(defaultTargetUrl),
-				"defaultTarget must start with '/' or with 'http(s)'");
-		this.defaultTargetUrl = defaultTargetUrl;
-	}
-
-	/**
-	 * If <code>true</code>, will always redirect to the value of {@code defaultTargetUrl}
-	 * (defaults to <code>false</code>).
-	 */
-	public void setAlwaysUseDefaultTargetUrl(boolean alwaysUseDefaultTargetUrl) {
-		this.alwaysUseDefaultTargetUrl = alwaysUseDefaultTargetUrl;
-	}
-
-	protected boolean isAlwaysUseDefaultTargetUrl() {
-		return alwaysUseDefaultTargetUrl;
-	}
-
-	/**
-	 * If this property is set, the current request will be checked for this a parameter
-	 * with this name and the value used as the target URL if present.
-	 *
-	 * @param targetUrlParameter the name of the parameter containing the encoded target
-	 * URL. Defaults to null.
-	 */
-	public void setTargetUrlParameter(String targetUrlParameter) {
-		if (targetUrlParameter != null) {
-			Assert.hasText(targetUrlParameter, "targetUrlParameter cannot be empty");
-		}
-		this.targetUrlParameter = targetUrlParameter;
-	}
-
-	protected String getTargetUrlParameter() {
-		return targetUrlParameter;
-	}
-	
-	public boolean isUseReferer() {
-		return useReferer;
-	}
-
-	public void setUseReferer(boolean useReferer) {
-		this.useReferer = useReferer;
-	}
-	
 	
 }
