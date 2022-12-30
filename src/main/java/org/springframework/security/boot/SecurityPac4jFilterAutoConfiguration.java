9b295519363aeb2e3319835a1dae5bc1f29e92aa
wandl <hiwepy@163.com>
2022-12-30 18:34:41 +0800

    2.0.2-SNAPSHOT

--- src/main/java/org/springframework/security/boot/SecurityPac4jFilterAutoConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityPac4jFilterAutoConfiguration.java
@@ -41,6 +41,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
